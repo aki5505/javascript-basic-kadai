3e68853ddee3bd949dbbfdd7be29a17757bb6e23
aki <akki.s1102@gmail.com>
2024-05-18 10:56:54 +0900

    jsファイルを修正

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,12 +1,13 @@
-num = Math.floor(Math.random() * 15);
+num = Math.floor(Math.random() * 16);
 
 
-if(num % 3 ===0){
-    console.log('3の倍数です');
+if(num % 3 ===0 && num % 5 ===0){
+    console.log('3と5の倍数です');
 }else if(num % 5 ===0){
     console.log('5の倍数です');
-}else if(num % 3 ===0 && num % 5 ===0){
-    console.log('3と5の倍数です');
+}else if(num % 3 ===0){
+    console.log('3の倍数です');
 }else{
     console.log(num);
 }
+
